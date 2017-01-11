394de799a64dd2bdf3c9071dc69da0d4bfb62a79
Mingling94 <mcluo1994@gmail.com>
2017-01-10 21:56:40 -0800

    Slimmed down the server.js

--- server.js
+++ server.js
@@ -1,32 +1,75 @@
-var restify = require('restify');
 var builder = require('botbuilder');
+var restify = require('restify');
 
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url); 
+});
+  
 // Get secrets from server environment
 var botConnectorOptions = { 
     appId: process.env.BOTFRAMEWORK_APPID, 
     appSecret: process.env.BOTFRAMEWORK_APPSECRET 
 };
 
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+// Create chat bot
+var connector = new builder.ChatConnector(botConnectorOptions);
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
+  'directory': '.',
+  'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+// Attempt to mimick echobot
+bot.dialog('/', function (session) {
+    //respond with user's message
+    session.send("You said " + session.message.text);
 });
+/*
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+  function (session) {
+    session.beginDialog('/profile');
+  },
+  function (session, results) {
+    session.send('Ok... Changed your name to %s', session.userData.name);
+  }
+]);
+
+intents.onDefault([
+  function (session, args, next) {
+    if (!session.userData.name) {
+      session.beginDialog('/profile');
+    } else {
+      next();
+    }
+  },
+  function (session, results) {
+    session.send('Hello %s!', session.userData.name);
+  }
+]);
+
+bot.dialog('/profile', [
+  function (session) {
+    builder.Prompts.text(session, 'Hi! What is your name?');
+  },
+  function (session, results) {
+    session.userData.name = results.response;
+    session.endDialog();
+  }
+]);
+*/
