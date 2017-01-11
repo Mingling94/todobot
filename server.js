01d24647ab274f54c216da577981a9be47d77b4d
Mingling94 <mcluo1994@gmail.com>
2017-01-10 21:15:57 -0800

    Mimick echobot static web page

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+  'directory': '.',
+  'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url); 
+});
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
